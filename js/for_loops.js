e7556a9261c0cfe42712c774eeccd551a53833a2
Austin Gray <Austin.m.gray210@gmail.com>
2020-02-12 12:11:46 -0600

    updated exercises

---
+++ js/for_loops.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// function showMultiplicationTable(x) {
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(x + " x " + i + " = " + (i * x));
+//     }
+// }
+// showMultiplicationTable(7);
+
+//
+// for (var i = 0; i < 10; i++) {
+//     var random = Math.floor(Math.random() * 180) + 20;
+//     if (random % 2 === 0) {
+//         console.log(random + " is even.");
+//     } else if (random % 2 !== 0) {
+//         console.log(random + " is odd.");
+//     }
+// }
+
+
+// for (var i = 0; i < 9; i++) {
+//     var triangleArray = ["1", "22", "333", "4444", "55555", "666666", "7777777", "88888888", "999999999"];
+//     console.log(triangleArray[i]);
+// }
+
+
+
+// for (var i = 100; i >= 5; i -= 5) {
+//     console.log(i);
+// }
+
+
