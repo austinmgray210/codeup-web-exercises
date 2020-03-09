306e814218c04dc87a3aaefd701bea8586540bb0
Austin Gray <Austin.m.gray210@gmail.com>
2020-03-09 11:12:44 -0500

    updated weather map

--- js/weather-map.js
+++ js/weather-map.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
         console.log(data);
     });
 
+
     mapboxgl.accessToken = mapboxToken;
     var map = new mapboxgl.Map({
         container: 'map',      // property is container, value is map //
@@ -14,5 +15,39 @@ $(document).ready(function () {
         zoom: 10,
         center: [-98.4916, 29.4252]
     });
+
+
+    var weatherIcons = {
+            "clear-day": "<img src='./weather-icons/sun.png' alt='day'>",
+            "clear-night": "<img src='./weather-icons/night.png' alt='night'>",
+            "rain": "<img src='./weather-icons/rain.png' alt='rain'>",
+            "snow": "<img src='./weather-icons/snow.png' alt='snow'>",
+            "sleet": "<img src='./weather-icons/sleet.png' alt='sleet'>",
+            "wind": "<img src='./weather-icons/wind.png' alt='wind'>",
+            "fog": "<img src='./weather-icons/fog.png' alt='fog'>",
+            "cloudy": "<img src='./weather-icons/cloudy.png' alt='clouds'>",
+            "partly-cloudy-day": "<img src='./weather-icons/sun-partly-cloudy.png' alt='p-c-day'>",
+            "partly-cloudy-night": "<img src='./weather-icons/night-partly-cloudy.png' alt='p-c-night'>"
+        };
+
+
+
+$('#todaysIcon').html(weatherIcons.rain);
+$('#tomorrowsIcon').html(weatherIcons.wind);
+$('#inTwoDaysIcon').html(weatherIcons["clear-day"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
